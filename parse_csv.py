369bf9da6090ff932fe8eadad9da27f08c7a6469
POK NGA HO <p.ho.22@abdn.ac.uk>
2023-02-24 21:29:10 +0000

    edit movie.csv to 2843 rows of data 10 years of movie 1986-1995

---
+++ parse_csv.py
@@ -0,0 +1,11 @@
+import csv
+import sqlite3
+
+conn.sqlite.connect('movie.db')
+cur=conn.cursor()
+
+conn.execute('DROP TABLE IF EXISTS movies')
+print("table dropped successfully");
+
+conn.execute('CREATE TABLE movies ()')
+print("table created successfully")
